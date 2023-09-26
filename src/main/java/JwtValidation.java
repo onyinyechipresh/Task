e73b62dc6e64bf503a40338d3d9d9ce566b90727
peaceonyinye <onyinyechipresh05@gmail.com>
2023-09-26 16:39:48 +0100

    modified jwt validation

--- src/main/java/JwtValidation.java
+++ src/main/java/JwtValidation.java
@@ -3,6 +3,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
@@ -13,42 +15,49 @@ public class JwtValidation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Username (min 4 characters): ");
+        System.out.print("Username: ");
         String username = scanner.nextLine();
-        System.out.print("Email (valid format): ");
+
+        System.out.print("Email: ");
         String email = scanner.nextLine();
+
         System.out.print("Password (min 8 characters, 1 upper case, 1 special character, 1 number): ");
         String password = scanner.nextLine();
+
         System.out.print("Date of Birth (YYYY-MM-DD): ");
-        String dob = scanner.nextLine();
+        String dateOfBirth = scanner.nextLine();
+        LocalDateTime dob = parseDateOfBirth(dateOfBirth);
 
         // todo:Validate all fields concurrently
-        CompletableFuture<Boolean> usernameValidation = CompletableFuture.supplyAsync(() -> isValidUsername(username));
-        CompletableFuture<Boolean> emailValidation = CompletableFuture.supplyAsync(() -> isValidEmail(email));
-        CompletableFuture<Boolean> passwordValidation = CompletableFuture.supplyAsync(() -> isValidPassword(password));
-        CompletableFuture<Boolean> dobValidation = CompletableFuture.supplyAsync(() -> isValidDOB(dob));
+        CompletableFuture<Validate> usernameValidation = CompletableFuture.supplyAsync(() -> isValidUsername(username));
+        CompletableFuture<Validate> emailValidation = CompletableFuture.supplyAsync(() -> isValidEmail(email));
+        CompletableFuture<Validate> passwordValidation = CompletableFuture.supplyAsync(() -> isValidPassword(password));
+        CompletableFuture<Validate> dobValidation = CompletableFuture.supplyAsync(() -> isValidDOB(String.valueOf(dob)));
 
         try{
             // todo:Combine the results and check if all validations passed
-            boolean isValid = usernameValidation.get() && emailValidation.get() && passwordValidation.get() && dobValidation.get();
+            boolean isValid = usernameValidation.get().isValid()
+                    && emailValidation.get().isValid()
+                    && passwordValidation.get().isValid()
+                    && dobValidation.get().isValid();
             if (isValid) {
                 // todo:Generate a JWT token upon successful validation
-                String jwt = generateJWT(username);
+                String jwt = generateJWT(email);
                 System.out.println("Registration Successful!");
                 System.out.println("JWT Token: " + jwt);
             } else {
                 System.out.println("Registration Failed. Validation Errors:");
-                if (!usernameValidation.get()) {
-                    System.out.println("Username: min 4 characters");
+                if (!usernameValidation.get().isValid()) {
+                    System.out.println(usernameValidation.get().getErrorMessages());
                 }
-                if (!emailValidation.get()) {
-                    System.out.println("Email: invalid format");
+                if (!emailValidation.get().isValid()) {
+                    System.out.println(emailValidation.get().getErrorMessages());
                 }
-                if (!passwordValidation.get()) {
-                    System.out.println("Password: invalid format");
+                if (!passwordValidation.get().isValid()) {
+                    System.out.println(passwordValidation.get().getErrorMessages());
                 }
-                if (!dobValidation.get()) {
-                    System.out.println("Date of Birth: invalid format or must be 16 years or older");
+                if (!dobValidation.get().isValid()) {
+                    System.out.println(dobValidation.get().getErrorMessages());
                 }
             }
 
@@ -59,37 +68,37 @@ public class JwtValidation {
     }
 
 
-    private static boolean isValidUsername(String username) {
+    private static Validate isValidUsername(String username) {
         // todo:Validation username (min 4 characters)
 
         Validate res = new Validate();
-        if (username.length() < 4) {
-            res.addError("Username: minimum of 4 characters");
+        if (username == null || username.length() < 4) {
+            res.addError("Username: should not be empty and must have at least 4 characters");
         }
-        return true;
+        return res;
     }
 
-    private static boolean isValidEmail(String email) {
+    private static Validate isValidEmail(String email) {
         // todo: Validate email (valid format)
         Validate res = new Validate();
-        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        if (!Pattern.matches(emailRegex, email)) {
-            res.addError("Email: invalid format");
+        String emailRegex = "^([\\w-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+        if (!Pattern.matches(emailRegex, email) || email == null) {
+            res.addError("Email: should not be empty and must be a valid email format");
         }
-        return true;
+        return res;
     }
 
-    private static boolean isValidPassword(String password) {
+    private static Validate isValidPassword(String password) {
         // todo:Validate password (min 8 characters, 1 upper case, 1 special character, 1 number)
         Validate res = new Validate();
         String passwordRegex = "^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$";
-        if (!Pattern.matches(passwordRegex, password)) {
-            res.addError("Password: invalid format");
+        if (!Pattern.matches(passwordRegex, password) || password == null) {
+            res.addError("Password: should not be empty and must be valid format");
         }
-        return true;
+        return res;
     }
 
-    private static boolean isValidDOB(String dob) {
+    private static Validate isValidDOB(String dob) {
         // todo:Validate password
         Validate res = new Validate();
         if (dob.isEmpty()) {
@@ -101,7 +110,7 @@ public class JwtValidation {
                     res.addError("Date of Birth: invalid format");
                 } else {
                     int year = Integer.parseInt(parts[0]);
-                    int currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+                    int currentYear = java.time.Year.now().getValue();
                     if (currentYear - year < 16) {
                         res.addError("Date of Birth: must be 16 years or older");
                     }
@@ -110,17 +119,26 @@ public class JwtValidation {
                 res.addError("Date of Birth: invalid format");
             }
         }
-        return true;
+        return res;
+    }
+
+
+    public static LocalDateTime parseDateOfBirth(String dobString) {
+        try {
+            return LocalDateTime.parse(dobString + "T00:00:00");
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
     // todo:Generate a JWT token
-    private static String generateJWT(String username) {
+    private static String generateJWT(String email) {
         Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         long currentTimeMillis = System.currentTimeMillis();
-        Date expiration = new Date(currentTimeMillis + 3600_000); // Token valid for 1 hour
+        Date expiration = new Date(currentTimeMillis + 3600_000);
 
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(email)
                 .setExpiration(expiration)
                 .signWith(key)
                 .compact();
